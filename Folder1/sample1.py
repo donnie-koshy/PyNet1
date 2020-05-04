35b44c1677deddddb9ccf462a9d417d155974ecd
Donnie Koshy <donnie.koshy@gmail.com>
2020-05-04 16:07:41 -0700

    Updating Sample#1 second time

--- Folder1/sample1.py
+++ Folder1/sample1.py
@@ -6,3 +6,10 @@ print("File for Branch Commit")
 for i in range(5):
     print (i)
 
+#By this time, folder level set of commits has been done.
+#Now we have modified this file again and do another commit.
+
+for x in range (5):
+    x*=2
+    print (x)
+ 
